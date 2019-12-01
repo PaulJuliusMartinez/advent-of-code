3afad69a4748eb262f395f4b821ff3c53f655285
Paul Julius Martinez <pauljuliusmartinez@gmail.com>
2019-11-30 23:17:07 -0600

    December 1st solution.

--- dec01.rb
+++ dec01.rb
@@ -1,1 +1,29 @@
-puts 'merry christmas!'
+#! /usr/bin/ruby
+
+masses = File.readlines('dec01.input').map(&:to_i)
+
+def fuel_for_mass(m)
+  (m / 3).floor - 2
+end
+
+
+### Star 1
+
+total = 0
+masses.each {|m| total += fuel_for_mass(m)}
+
+puts total
+
+
+### Star 2
+
+total = 0
+masses.each do |m|
+  fuel_mass = fuel_for_mass(m)
+  while fuel_mass > 0
+    total += fuel_mass
+    fuel_mass = fuel_for_mass(fuel_mass)
+  end
+end
+
+puts total
