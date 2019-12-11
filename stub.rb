eb019818c2377bd642480c520eb643b91d6c5d4c
Paul Julius Martinez <pauljuliusmartinez@gmail.com>
2019-12-10 21:14:31 -0800

    December 11th solutions.

--- stub.rb
+++ stub.rb
@@ -5,7 +5,7 @@ require './input.rb'
 
 strs = get_input_str_arr(__FILE__)
 str = get_input_str(__FILE__)
-ints1 = get_single_line_input_int_arr(__FILE__)
+ints1 = get_single_line_input_int_arr(__FILE__, separator: ',')
 ints2 = get_multi_line_input_int_arr(__FILE__)
 
 puts strs.inspect
