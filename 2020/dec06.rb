7b198ddb847b9dfc752d34f0f0415e4acde88a6a
Paul Julius Martinez <pauljuliusmartinez@gmail.com>
2020-12-05 21:15:53 -0800

    December 6th, 2020 solution

---
+++ 2020/dec06.rb
@@ -0,0 +1,50 @@
+#! /usr/bin/env ruby
+
+require './input.rb'
+
+require 'set'
+require 'prime'
+require 'scanf'
+
+strs = get_input_str_arr(__FILE__)
+
+total_count = 0
+seen = Set.new
+
+strs.each do |str|
+  if str == ''
+    total_count += seen.count
+    seen = Set.new
+    next
+  end
+
+  str.chars.each do |ch|
+    seen << ch
+  end
+end
+
+total_count += seen.count
+
+puts "Part 1: #{total_count}"
+
+total_count = 0
+seen = Hash.new {|h, k| h[k] = 0}
+people = 0
+
+strs.each do |str|
+  if str == ''
+    total_count += seen.values.count {|v| v == people}
+    seen = Hash.new {|h, k| h[k] = 0}
+    people = 0
+    next
+  end
+
+  str.chars.each do |ch|
+    seen[ch] += 1
+  end
+  people += 1
+end
+
+total_count += seen.values.count {|v| v == people}
+
+puts "Part 2: #{total_count}"
